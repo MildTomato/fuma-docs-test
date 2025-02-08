eabc8761a40a7d4e38c7ffd79c0774c613631a3d
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2025-02-08 16:42:47 +0800

    add example pages

--- scripts/generate-docs.mjs
+++ scripts/generate-docs.mjs
@@ -2,5 +2,5 @@ import { generateFiles } from "fumadocs-openapi";
 
 void generateFiles({
   input: ["./spec/api_v1_openapi.json"], // the OpenAPI schemas
-  output: "./content/management-api",
+  output: "./content/docs/management-api",
 });
