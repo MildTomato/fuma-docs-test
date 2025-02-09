ebd612aa643d0b7d11e2094390cdeb37342ec374
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2025-02-09 10:51:23 +0800

    fix layout overflow issues

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -180,6 +180,7 @@ export function Sidebar({
         blockScrollingWidth={768} // md
         {...props}
         className={cn(
+          "sticky top-[calc(var(--fd-banner-height)+var(--fd-nav-height))]",
           //   "overflow-y-auto",
           "bg-fd-card text-sm md:h-[var(--fd-sidebar-height)]",
           "max-md:inset-x-0 max-md:bottom-0 max-md:bg-fd-background/80 max-md:text-[15px] max-md:backdrop-blur-lg max-md:data-[open=false]:invisible",
